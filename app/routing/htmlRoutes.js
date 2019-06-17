20e04cd055e48e7c2027e3f7ad84e015ffd68177
Jesse Chen <811337@gmail.com>
2019-06-16 22:32:57 -0500

    working app

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,16 @@
-var path = require("path");
+var path = require('path');
 
+// Export HTML routes
 module.exports = function(app) {
-	// if user enters survey in URL or presses survey button, serves the survey HTML file
-	app.get("/survey", function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/survey.html"));
+	// console.log('___ENTER htmlRoutes.js___');
+
+	// Home page
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 
-	// fallback use route for homepage
-	app.use(function(req, res) {
-		res.sendFile(path.join(__dirname, "/../public/home.html"));
+	// Survey page
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
 	});
 };
--- server.js
+++ server.js
@@ -4,9 +4,8 @@ var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
-// For serving of static CSS
 app.use(express.static(__dirname + "/app/css"));
 
 app.use(bodyParser.json());
@@ -14,7 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// API and HTML routes
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
